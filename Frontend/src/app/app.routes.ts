e4f6169b42d5b5ca80b288d9cdb8b545880f7a6d
Daniel Piñol <daniel.pinol@ufm.edu>
2026-05-23 19:56:56 -0600

    Home page and background

---
+++ Frontend/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path: '', loadComponent: () => import('./Componentes/homepage/homepage').then(m => m.Homepage)},
+    {path: 'login', loadComponent: () => import('./Componentes/login/login').then(m => m.Login)},
+    {path: 'albums', loadComponent: () => import('./Componentes/albums/albums').then(m => m.Albums)},
+    {path: 'gallery', loadComponent: () => import('./Componentes/gallery/gallery').then(m => m.Gallery)},
+    {path: 'timeline', loadComponent: () => import('./Componentes/timeline/timeline').then(m => m.Timeline)},
+    {path: 'stats', loadComponent: () => import('./Componentes/stats/stats').then(m => m.Stats)},
+    {path: 'upload', loadComponent: () => import('./Componentes/upload/upload').then(m => m.Upload)},
+    {path: '**', redirectTo: '' }
+];
